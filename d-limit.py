80e3ebe39a21a7e8e2c6ac6bb1ef52c9130dcf32
lab <106988449+lab-104@users.noreply.github.com>
2022-12-06 19:16:24 +0530

    Update d-limit.py

--- d-limit.py
+++ d-limit.py
@@ -1,16 +1,20 @@
+
 #Depth Limit Search
 v=14
 graph=[[] for i in range(v)]
 visited=[False for i in range(v)]
 def DFS(s,dest,cur_depth,depth,visited):
   if s==dest:
-    print(s, '-destination',end=' ')
-    return
+    print(s, '-',end=' ')
+    return True
   visited[s]=True
   print(s,end=' ')
   for p in graph[s]:
     if visited[p]==False and cur_depth<depth:
-      DFS(p,dest,cur_depth+1,depth,visited)
+      if DFS(p,dest,cur_depth+1,depth,visited)==True:
+        return True
+  return False
+  
 
 def addedge(a,b):
   graph[a].append(b)
@@ -18,17 +22,16 @@ def addedge(a,b):
 
 addedge(0, 1)
 addedge(0, 2)
-addedge(0, 3)
 addedge(1, 4)
 addedge(1, 5)
 addedge(2, 6)
 addedge(2, 7)
-addedge(3, 8)
-addedge(8, 9)
-addedge(8, 10)
+addedge(4, 9)
+addedge(4, 10)
 addedge(9, 11)
 addedge(9, 12)
-addedge(9, 13)
+addedge(10, 13)
+
 source=int(input("Enter source : "))
 destination=int(input("Enter destination : "))
 depth=int(input('Enter depth Limit: '))
